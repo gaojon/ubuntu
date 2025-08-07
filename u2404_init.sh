b5681d328c584b2c499fa89dfab247bcb3094142
gaojon <gaojon@hotmail.com>
2025-08-07 10:33:55 +0800

    Update u2404_init.sh

--- u2404_init.sh
+++ u2404_init.sh
@@ -19,3 +19,8 @@ apt -y install tigervnc-standalone-server
 
 echo "Add a alias to start vncserver"
 echo "alias vncs='tigervncserver -xstartup /usr/bin/gnome-session -SecurityTypes VncAuth,TLSVnc -geometry 1980x1020 -localhost no :58'" | sudo tee -a /etc/bash.bashrc
+
+echo "Need to reboot before launch vncserver"
+echo "Only works when start vncserver from SSH terminal"
+echo "It's safe to ignore the error in vnc log file:"
+echo "    libEGL warning: failed to open /dev/dri/card1: Permission denied"
