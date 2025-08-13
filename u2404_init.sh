21f1716a80a2b728cae6155dddb5c34e60a36ccd
gaojon <gaojon@hotmail.com>
2025-08-13 15:22:17 +0800

    Update u2404_init.sh

--- u2404_init.sh
+++ u2404_init.sh
@@ -19,21 +19,10 @@ echo "#######################    Enable sshd    #########################"
 apt -y install openssh-server -qq
 systemctl restart ssh
 
-echo "#######################  Install docker    ######################"
+echo "#######################  Install GPU driver    ######################"
 
-apt-get update
-apt-get install ca-certificates curl -y -qq
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -qq
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y -qq
+apt install "linux-headers-$(uname -r)" "linux-modules-extra-$(uname -r)" -q -y
+apt install amdgpu-dkms -q -y
 
 echo "#######################  config docker repo   ######################"
 systemctl stop docker
